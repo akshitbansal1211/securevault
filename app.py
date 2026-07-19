dc1f949961480519dc90d6dee974970c516ecbc9
Akshit <bansalakshit1211@gmail.com>
2026-07-19 19:51:33 +0530

    Day 6: Add login form (frontend) and HTTP deep-dive

--- app.py
+++ app.py
@@ -37,6 +37,10 @@ def register():
 
     return render_template("register.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
